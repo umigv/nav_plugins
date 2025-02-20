afef47f9d145478136464e74ab27e7797284720d
Ryan <ryan.liao0305@gmail.com>
2025-02-20 16:26:06 -0500

    Fix straight line planner copy paste error

--- planner/straight_line_planner/src/straight_line_planner.cpp
+++ planner/straight_line_planner/src/straight_line_planner.cpp
@@ -5,12 +5,12 @@
 using namespace infra_common;
 using namespace planner_plugins;
 
-std::vector<CellCoordinate> ExamplePlanner::find_path(const Costmap &costmap, 
+std::vector<CellCoordinate> StraightLinePlanner::find_path(const Costmap &costmap, 
     const std::function<bool(int)> &drivable,
     const CellCoordinate &start,
     const CellCoordinate &goal) 
 {
-RCLCPP_INFO(rclcpp::get_logger("ExamplePlanner"), "ExamplePlanner finding path");  
+RCLCPP_INFO(rclcpp::get_logger("StraightLinePlanner"), "StraightLinePlanner finding path");  
 
 // Straight line planner: just goes up in a straight line until it reaches the goal
 // or hits an obstacle
