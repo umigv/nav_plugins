00140e55b22e88a2483f315e06426fdbf0855424
Ryan <ryan.liao0305@gmail.com>
2025-03-19 16:47:56 -0400

    Minor style changes

--- controller/pure_pursuit_controller/include/pure_pursuit_controller.hpp
+++ controller/pure_pursuit_controller/include/pure_pursuit_controller.hpp
@@ -9,10 +9,10 @@ class PurePursuitController final : public plugin_base_classes::Controller {
 public:
     PurePursuitController();
 
-    void set_path(const std::vector<geometry_msgs::msg::Point> &path) override;
+    void set_path(const std::vector<geometry_msgs::msg::Point>& path) override;
 
-    auto compute_next_command_velocity(const geometry_msgs::msg::Pose &current_pose, 
-                                       [[maybe_unused]] const geometry_msgs::msg::Twist &current_velocity) 
+    auto compute_next_command_velocity(const geometry_msgs::msg::Pose& current_pose, 
+                                       [[maybe_unused]] const geometry_msgs::msg::Twist& current_velocity) 
         -> geometry_msgs::msg::Twist override;
 
     auto is_finished() const -> bool override;
