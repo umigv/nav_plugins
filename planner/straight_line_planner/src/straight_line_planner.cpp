ade26c57478b0e7c9f69490ed76a16b1f6d09de9
John Rose <johnrose@umich.edu>
2025-03-23 15:03:10 -0400

    StraightLinePlanner goes in x direction instead of y direction

--- planner/straight_line_planner/src/straight_line_planner.cpp
+++ planner/straight_line_planner/src/straight_line_planner.cpp
@@ -10,28 +10,31 @@ std::vector<CellCoordinate> StraightLinePlanner::find_path(const Costmap &costma
     const CellCoordinate &start,
     const CellCoordinate &goal) 
 {
-RCLCPP_INFO(rclcpp::get_logger("StraightLinePlanner"), "StraightLinePlanner finding path");  
+    RCLCPP_INFO(rclcpp::get_logger("StraightLinePlanner"), "StraightLinePlanner finding path");  
 
-// Straight line planner: just goes up in a straight line until it reaches the goal
-// or hits an obstacle
-std::vector<CellCoordinate> path;
-path.push_back(start);
-CellCoordinate curr = start;
-while (true) 
-{
-    if (curr == goal)
+    // Straight line planner: just goes in the positive x direction until it reaches the goal
+    // or hits an obstacle
+    std::vector<CellCoordinate> path;
+    path.push_back(start);
+    CellCoordinate curr = start;
+    while (true) 
     {
-        break;
+        if (curr == goal)
+        {
+            break;
+        }
+        curr.x += 1;
+        RCLCPP_INFO(rclcpp::get_logger("StraightLinePlanner"), "Cost at cell: %d, %d is %d", curr.x, curr.y, costmap.GetCost(curr.x, curr.y));
+        if (!costmap.InBounds(curr.x, curr.y) ||
+            !drivable(costmap.GetCost(curr.x, curr.y))) 
+        {
+            RCLCPP_INFO(rclcpp::get_logger("StraightLinePlanner"), "Hit obstacle or went out-of-bounds at cell: %d, %d", curr.x, curr.y);    
+            break;
+        }
+        path.push_back(curr);
+        RCLCPP_INFO(rclcpp::get_logger("StraightLinePlanner"), "Adding cell to path: %d, %d", curr.x, curr.y);  
     }
-    curr.y += 1;
-    if (!costmap.InBounds(curr.x, curr.y) ||
-        !drivable(costmap.GetCost(curr.x, curr.y))) 
-    {
-        break;
-    }
-    path.push_back(curr);
-}
 
-return path;
+    return path;
 }
 
